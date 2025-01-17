933707a05f4f9a955781175105767297ff1ce91b
GeorgeF37 <sinester66@yahoo.com>
2025-01-17 14:58:19 +0000

    Base game screen created

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from constants import *
 def main():
     pygame.init()
     print("Starting asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -16,10 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        #print(f'Screen width: {SCREEN_WIDTH}')
-        #print(f'Screen height: {SCREEN_HEIGHT}')
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

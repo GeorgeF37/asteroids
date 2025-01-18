9c4766f9cd8fce7a237b676ab1d857a3159b17a7
GeorgeF37 <sinester66@yahoo.com>
2025-01-18 22:45:07 +0000

    Added the player to the screen

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -13,12 +14,16 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    the_player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill('black')
+        the_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
